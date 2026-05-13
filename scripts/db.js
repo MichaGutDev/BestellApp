77017f875e49ff7a281cb7bf973ef557ff9326a5
Micha Gut <m.gut@windowslive.com>
2026-05-13 07:01:43 +0200

    feat: create and populate db.js

---
+++ scripts/db.js
@@ -0,0 +1,111 @@
+let dishes = [
+
+    //BURGER
+  {
+    name: "Veggie mushroom black burger",
+    category: "burger",
+    imageSource: "./assets/img/burger/black-burger.jpg",
+    imageAlt: "Black Burger",
+    price: 16.9,
+    ingredients: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+  },
+  {
+    name: "All meat burger",
+    category: "burger",
+    imageSource: "./assets/img/burger/all-meat-burger.jpg",
+    imageAlt: "All Meat Burger",
+    price: 15.9,
+    ingredients: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ sauce",
+  },
+  {
+    name: "Beef red burger",
+    category: "burger",
+    imageSource: "./assets/img/burger/beef-red-burger.jpg",
+    imageAlt: "Beef Red Burger",
+    price: 14.9,
+    ingredients: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+  },
+  {
+    name: "Big chicken burger",
+    category: "burger",
+    imageSource: "./assets/img/burger/big-chicken-burger.jpg",
+    imageAlt: "Big Chicken Burger",
+    price: 15.9,
+    ingredients: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper",
+  },
+
+  //PIZZA
+
+  {
+    name: "Pizza Margherita",
+    category: "pizza",
+    imageSource: "./assets/img/pizza/pizza-margherita.jpg",
+    imageAlt: "Pizza Margherita",
+    price: 11.9,
+    ingredients: "Tomato Sauce, Mozzarella",
+  },
+  {
+    name: "Pizza Chorizo",
+    category: "pizza",
+    imageSource: "./assets/img/pizza/pizza-chorizo.jpg",
+    imageAlt: "Pizza Chorizo",
+    price: 13.9,
+    ingredients: "Tomato slices, Mozzarella, Chorizo",
+  },
+  {
+    name: "Funghi",
+    category: "pizza",
+    imageSource: "./assets/img/pizza/pizza-funghi.jpg",
+    imageAlt: "Pizza Funghi",
+    price: 12.9,
+    ingredients: "Red onion, Olives, Button Mushrooms, Mozzarella",
+  },
+  {
+    name: "Quattro Formaggi with Chicken",
+    category: "pizza",
+    imageSource: "./assets/img/pizza/pizza-quattro-formaggi.jpg",
+    imageAlt: "Pizza Quattro Formaggi with Chicken",
+    price: 15.9,
+    ingredients: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+  },
+
+  //SALAD
+
+  {
+    name: "Warm beef arugula salad",
+    category: "salad",
+    imageSource: "./assets/img/salad/warm-beef-arugula-salad.jpg",
+    imageAlt: "Warm Beef Salad",
+    price: 16.9,
+    ingredients:
+      "Beef, Arugula, Field salad, Greek feta, Cherry tomatoes, Sun-dried Tomatoes, Balsamic-vinegar dressing",
+  },
+  {
+    name: "Mini green Salad",
+    category: "salad",
+    imageSource: "./assets/img/salad/mini-green-salad.jpg",
+    imageAlt: "Mini Green Salad",
+    price: 17.9,
+    ingredients: "Green salad, Cucumber, Carrots, Parsley, Radishes",
+  },
+  {
+    name: "Green Salad with sea food",
+    category: "salad",
+    imageSource: "./assets/img/salad/green-salad-with-sea-food.jpg",
+    imageAlt: "Green Salad with Seafood",
+    price: 16.9,
+    ingredients:
+      "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+  },
+  {
+    name: "Vegan green salad with tofu",
+    category: "salad",
+    imageSource: "./assets/img/salad/vegan-green-salad.jpg",
+    imageAlt: "Vegan green salad with tofu",
+    price: 14.9,
+    ingredients:
+      "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+  },
+];
+
+let basketItems = [];
